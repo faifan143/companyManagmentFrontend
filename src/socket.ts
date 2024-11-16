c1f8e5bd8d7745d29deddcc28cfb6856543cf2f4
Besher Hamze <136814619+Besher-Hamze@users.noreply.github.com>
2024-11-16 14:32:11 +0300

    Update socket.ts URL

--- src/socket.ts
+++ src/socket.ts
@@ -1,10 +1,7 @@
 import { io } from "socket.io-client";
 import Cookies from "js-cookie";
 // "undefined" means the URL will be computed from the `window.location` object
-const URL =
-  process.env.NODE_ENV === "production"
-    ? undefined
-    : `http://ec2-35-173-139-149.compute-1.amazonaws.com/`;
+const URL = `http://${process.env.BASE_URL}`;
 
 export const socket = io(URL, {
   extraHeaders: {
